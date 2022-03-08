c34fd5aecc9cc12010b885cde6afab5c5de8b67a
kurniawa <kurniawan_01@ymail.com>
2022-03-08 14:39:40 +0700

    first commit

---
+++ app/Http/Controllers/EkspedisiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ekspedisi;
+use App\Http\Requests\StoreEkspedisiRequest;
+use App\Http\Requests\UpdateEkspedisiRequest;
+
+class EkspedisiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEkspedisiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEkspedisiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ekspedisi  $ekspedisi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ekspedisi $ekspedisi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ekspedisi  $ekspedisi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ekspedisi $ekspedisi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateEkspedisiRequest  $request
+     * @param  \App\Models\Ekspedisi  $ekspedisi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateEkspedisiRequest $request, Ekspedisi $ekspedisi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ekspedisi  $ekspedisi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ekspedisi $ekspedisi)
+    {
+        //
+    }
+}
