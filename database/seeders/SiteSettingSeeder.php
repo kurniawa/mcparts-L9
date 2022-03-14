9f6e7a724691a2e0717145ca1b12332c07b905ff
kurniawa <kurniawan_01@ymail.com>
2022-03-14 10:55:03 +0700

    Masih utak atik sitesetting seeder

--- database/seeders/SiteSettingSeeder.php
+++ database/seeders/SiteSettingSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\SiteSetting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SiteSettingSeeder extends Seeder
 {
@@ -18,8 +19,14 @@ class SiteSettingSeeder extends Seeder
         $site_settings = [
             ['setting' => 'load_num', 'value' => 0]
         ];
+        // for ($i = 0; $i < count($site_settings); $i++) {
+        //     SiteSetting::create([
+        //         'setting' => $site_settings[$i]['setting'],
+        //         'value' => $site_settings[$i]['value'],
+        //     ]);
+        // }
         for ($i = 0; $i < count($site_settings); $i++) {
-            SiteSetting::create([
+            DB::table('site_settings')->insert([
                 'setting' => $site_settings[$i]['setting'],
                 'value' => $site_settings[$i]['value'],
             ]);
